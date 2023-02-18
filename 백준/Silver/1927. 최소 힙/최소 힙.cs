ca4cb14d0720fc96805a3b89bd217e2c02fa0d46
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-02-18 13:28:33 +0900

    [Silver II] Title: 최소 힙, Time: 108 ms, Memory: 11120 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Text;
+
+namespace MyTestingField
+{
+    class MinHeap
+    {
+        private long[] arr;
+        public int Count;
+        public MinHeap()
+        {
+            arr = new long[100002];
+            Count = 0;
+        }
+        public void Push(long value)
+        {
+            Count++;
+            arr[Count] = value;
+            int index = Count;
+            while(index > 1 && arr[index / 2] > arr[index])
+            {
+                long temp = arr[index / 2];
+                arr[index / 2] = arr[index];
+                arr[index] = temp;
+                index /= 2;
+            }
+        }
+        public long Pop()
+        {
+            if (Count == 0)
+                return 0;
+            long returnValue = arr[1];
+            arr[1] = arr[Count];
+            Count--;
+            int index = 1;
+            while(index * 2 <= Count)
+            {
+                int minIndex = 0;
+                if (index * 2 == Count)
+                    minIndex = index * 2;
+                else
+                    minIndex = arr[index * 2] > arr[index * 2 + 1] ? index * 2 + 1 : index * 2;
+                if (arr[index] > arr[minIndex])
+                {
+                    long temp = arr[index];
+                    arr[index] = arr[minIndex];
+                    arr[minIndex] = temp;
+                    index = minIndex;
+                }
+                else break;
+            }
+
+            return returnValue;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            MinHeap minHeap = new MinHeap();
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < n; i++)
+            {
+                long x = long.Parse(Console.ReadLine());
+                if (x == 0)
+                    sb.AppendLine(minHeap.Pop().ToString());
+                else
+                    minHeap.Push(x);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
