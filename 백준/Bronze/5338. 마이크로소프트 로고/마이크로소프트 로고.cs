c4db1e04d6052c2f0b987ddab28608b8b6d57bcb
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-03-10 05:01:05 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 56 ms, Memory: 5332 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,16 @@
+
+namespace TestingField
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = @"       _.-;;-._
+'-..-'|   ||   |
+'-..-'|_.-;;-._|
+'-..-'|   ||   |
+'-..-'|_.-''-._|";
+            Console.WriteLine(result);
+        }
+    }
+}
