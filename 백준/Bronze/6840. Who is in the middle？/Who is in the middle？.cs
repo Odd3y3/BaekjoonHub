4a96c3a7e99280e02e4d0d993dc563cb1684aba3
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-03-15 00:40:17 +0900

    [Bronze V] Title: Who is in the middle?, Time: 64 ms, Memory: 5556 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.cs
@@ -0,0 +1,6 @@
+List<int> list = new List<int>();
+list.Add(int.Parse(Console.ReadLine()));
+list.Add(int.Parse(Console.ReadLine()));
+list.Add(int.Parse(Console.ReadLine()));
+list.Sort();
+Console.WriteLine(list[1]);
