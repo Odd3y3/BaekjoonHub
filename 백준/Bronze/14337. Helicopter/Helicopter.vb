f8aeef7926dbb30d8c500548b03d296203eb3dcc
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-03-15 03:06:44 +0900

    [Bronze V] Title: Helicopter, Time: 56 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,13 @@
+Imports System
+
+Module Module1
+   Sub Main()
+     Console.WriteLine("      ===================
+          ____||___
+\ /      /       []\
+ X=======           \__
+/ \      \____________|
+            ||  ||
+         \-----------/")
+   End Sub
+End Module
