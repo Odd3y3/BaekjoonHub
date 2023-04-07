b3c4699ffd1e1a43ffca347d231de56dccb76159
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-04-08 03:56:42 +0900

    [Bronze IV] Title: 심부름 가는 길, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/5554. 심부름 가는 길/심부름 가는 길.cs
@@ -0,0 +1,7 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+int second = a + b + c + d;
+Console.WriteLine(second / 60);
+Console.WriteLine(second % 60);
