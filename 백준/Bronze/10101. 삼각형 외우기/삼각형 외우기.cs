1db522127b2fe6c43277d307768a542f5a79f38d
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-04-08 07:10:29 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 60 ms, Memory: 5452 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cs
@@ -0,0 +1,14 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+if (a == 60 && b == 60 && c == 60)
+    Console.WriteLine("Equilateral");
+else if (a + b + c == 180)
+{
+    if (a == b || b == c || c == a)
+        Console.WriteLine("Isosceles");
+    else
+        Console.WriteLine("Scalene");
+}
+else
+    Console.WriteLine("Error");
