74b687e0ce95103223e37550e6537490e8ff55d4
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-07-09 14:36:56 +0900

    [Gold I] Title: 수들의 합 7, Time: 2384 ms, Memory: 89024 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cs
@@ -0,0 +1,130 @@
+
+using System.Text;
+
+namespace TestingField
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            long[] arr = new long[n];
+
+            //process
+            SegmentTree st = new SegmentTree(arr);
+
+            //output
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < m; ++i)
+            {
+                int[] inputNum = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (inputNum[0] == 0)   //Sum함수
+                {
+                    int start = inputNum[1] - 1;
+                    int end = inputNum[2] - 1;
+                    if (start > end)
+                    {
+                        start = start ^ end;
+                        end = start ^ end;
+                        start = start ^ end;
+                    }
+                    sb.AppendLine(st.GetSum(start, end).ToString());
+                }
+                else if (inputNum[0] == 1)  //Modify 함수
+                {
+                    int numIndex = inputNum[1] - 1;
+                    int value = inputNum[2];
+
+                    if(numIndex >= 0 && numIndex < arr.Length)
+                    {
+                        st.ChangeNum(numIndex, arr[numIndex], value);
+                        arr[numIndex] = value;
+                    }
+                }
+            }
+
+            Console.WriteLine(sb);
+        }   
+    }
+
+    class SegmentTree
+    {
+        Node[] tree;
+
+        public SegmentTree(long[] array)
+        {
+            tree = new Node[array.Length * 4];
+            Init(1, 0, array.Length - 1, array);
+        }
+
+        private long Init(int index, int start, int end, long[] array)
+        {
+            if(start == end)
+            {
+                tree[index] = new Node(start, end, array[start]);
+                return array[start];
+            }
+
+            int middleIndex = (start + end) / 2;
+            long sum = Init(index * 2, start, middleIndex, array) + Init(index * 2 + 1, middleIndex + 1, end, array);
+            tree[index] = new Node(start, end, sum);
+            return sum;
+        }
+
+        public long GetSum(int start, int end)
+        {
+            return RecursiveGetSum(1, start, end);
+        }
+
+        private long RecursiveGetSum(int index, int start, int end)
+        {
+            Node node = tree[index];
+            if (node.Start >= start && node.End <= end)
+                return node.Sum;
+            if (node.Start > end || node.End < start)
+                return 0;
+
+            long leftValue = RecursiveGetSum(index * 2, start, end);
+            long rightValue = RecursiveGetSum(index * 2 + 1, start, end);
+
+            return leftValue + rightValue;
+        }
+
+        public void ChangeNum(int numIndex, long originValue, long value)
+        {
+            RecursiveChangeNum(1, numIndex, originValue, value);
+        }
+
+        private void RecursiveChangeNum(int index, int numIndex, long originValue, long value)
+        {
+            Node node = tree[index];
+            if(node.Start <= numIndex && node.End >= numIndex)
+            {
+                Node newNode = new Node(node.Start, node.End, node.Sum + value - originValue);
+                tree[index] = newNode;
+
+                
+                if(node.Start != node.End)
+                {
+                    RecursiveChangeNum(index * 2, numIndex, originValue, value);
+                    RecursiveChangeNum(index * 2 + 1, numIndex, originValue, value);
+                }
+            }
+        }
+    }
+
+    struct Node
+    {
+        public int Start { get; }
+        public int End { get; }
+        public long Sum { get; }
+        public Node(int start, int end, long sum)
+        {
+            this.Start = start;
+            this.End = end;
+            this.Sum = sum;
+        }
+    }
+}
