b45f703725ef9144a6cda9fb84ef5ce4e1f6445a
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-07-18 14:47:55 +0900

    [unrated] Title: 커피 심부름, Time: 0.33 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(string[] order) {
+        int answer = 0;
+        foreach (string str in order){
+            if(str.Contains("cafelatte"))
+                answer += 5000;
+            else
+                answer += 4500;
+        }
+        return answer;
+    }
+}
