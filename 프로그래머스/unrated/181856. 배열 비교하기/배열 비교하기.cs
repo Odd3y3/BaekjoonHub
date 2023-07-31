9fd4bb83087615c2135a69cc3d75a4113a934e18
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-07-31 09:04:09 +0900

    [unrated] Title: 배열 비교하기, Time: 0.20 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        if(arr1.Length > arr2.Length){
+            return 1;
+        }
+        else if(arr1.Length < arr2.Length){
+            return -1;
+        }
+        else{
+            if(arr1.Sum() > arr2.Sum()){
+                return 1;
+            }
+            else if(arr1.Sum() < arr2.Sum()){
+                return -1;
+            }
+            else
+                return 0;
+        }
+    }
+}
