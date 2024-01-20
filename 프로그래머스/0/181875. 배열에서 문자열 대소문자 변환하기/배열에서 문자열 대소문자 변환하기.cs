9613986a31c9be506295a7abcd11ddff4d8dfbe2
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-01-21 01:10:24 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.54 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string[] solution(string[] strArr) {
+        for(int i = 0; i < strArr.Length; ++i)
+        {
+            if(i % 2 != 0)
+                strArr[i] = strArr[i].ToUpper();
+            else
+                strArr[i] = strArr[i].ToLower();
+        }
+        return strArr;
+    }
+}
