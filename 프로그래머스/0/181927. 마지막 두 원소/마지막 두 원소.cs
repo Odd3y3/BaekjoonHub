2b7d805bce07e0ecd0f3b5989f554a1c4132f5ea
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-01-20 20:11:28 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.22 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.Length;
+        int[] answer = new int[len + 1];
+        for(int i = 0; i < len; ++i)
+        {
+            answer[i] = num_list[i];
+        }
+        if(answer[len - 1] > answer[len - 2])
+            answer[len] = answer[len - 1] - answer[len - 2];
+        else
+            answer[len] = answer[len - 1] * 2;
+        return answer;
+    }
+}
