334a8ce4a2037a9ccefd827ff50d2b6bd586ccb2
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-03-30 23:45:40 +0900

    [Bronze V] Title: Rectangles, Time: 80 ms, Memory: 5544 KB -BaekjoonHub

---
+++ 백준/Bronze/15232. Rectangles/Rectangles.cs
@@ -0,0 +1,10 @@
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+for(int i = 0; i < a; i++)
+{
+    for(int j = 0; j < b; j++)
+    {
+        Console.Write('*');
+    }
+    Console.WriteLine();
+}
