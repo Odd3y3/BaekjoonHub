e51d90c6e8fedb19b3c7b0344262b2d5bbeb3856
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-04-05 20:29:08 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.84 ms, Memory: 31.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Solution {
+    public int solution(string binomial) {
+        string[] arr = binomial.Split();
+        int a = int.Parse(arr[0]);
+        int b = int.Parse(arr[2]);
+        switch(arr[1])
+        {
+            case "+":
+                return a + b;
+                break;
+            case "-":
+                return a - b;
+                break;
+            case "*":
+                return a * b;
+                break;
+        }
+        return 0;
+    }
+}
