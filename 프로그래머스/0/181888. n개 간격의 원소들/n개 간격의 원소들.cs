0b8b6d746b976961588ae5af0416ba7b77c905d4
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-04-05 19:43:18 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.53 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<int> list = new List<int>();
+        for(int i = 0; i < num_list.Length; i += n)
+        {
+            list.Add(num_list[i]);
+        }
+        return list.ToArray();
+    }
+}
