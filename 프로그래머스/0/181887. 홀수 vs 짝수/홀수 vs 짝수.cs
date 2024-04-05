21f195da3759242128d29a0e4c0a5c5e1488e7d7
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-04-05 19:58:35 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.19 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int evenSum = 0;
+        int oddSum = 0;
+        for(int i = 0; i < num_list.Length; i++)
+        {
+            if(i % 2 == 0)
+                evenSum += num_list[i];
+            else
+                oddSum += num_list[i];
+        }
+        return evenSum > oddSum ? evenSum : oddSum;
+    }
+}
