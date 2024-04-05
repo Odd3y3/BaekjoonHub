11e5221316d21aa7be36225f0d8ffc13abda99a8
OddEye <110804298+Odd3y3@users.noreply.github.com>
2024-04-05 20:39:18 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.67 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] arr, int[,] intervals) {
+        List<int> list = new List<int>();
+        for(int i = intervals[0, 0]; i <= intervals[0, 1]; i++)
+        {
+            list.Add(arr[i]);
+        }
+        for(int i = intervals[1, 0]; i <= intervals[1, 1]; i++)
+        {
+            list.Add(arr[i]);
+        }
+        return list.ToArray();
+    }
+}
